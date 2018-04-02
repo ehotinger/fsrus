b5deacda1dc2ad1653b4b8b9fd63698c59325298
Eric Hotinger <erhoting@microsoft.com>
2018-04-02 13:19:28 -0700

    Update README, add default path tests

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # fsrus
 
-A simple filesystem hook for [sirupsen/logrus](https://github.com/sirupsen/logrus). Log to files with logrus.
+A simple filesystem hook for [sirupsen/logrus](https://github.com/sirupsen/logrus).
+Log to files with logrus.
+
+## Examples:
+
+
+```golang
+logger := logrus.New()
+logger.Level = logrus.DebugLevel
+hook, err := fsrus.NewFilesystemHook(nil, "output.txt", nil, nil)
+logger.AddHook(hook)
+logger.Info(expectedMsg)
+```
--- fsrus_test.go
+++ fsrus_test.go
@@ -11,6 +11,56 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// TestWritingToLog_DefaultPath tests writing all levels to a default path.
+func TestWritingToLog_DefaultPath(t *testing.T) {
+	expectedMsg := "This is expected output"
+
+	logger := logrus.New()
+	logger.Level = logrus.DebugLevel
+
+	tmpFile, err := ioutil.TempFile("", "test.txt")
+	fileName := tmpFile.Name()
+
+	defer func() {
+		tmpFile.Close()
+		os.Remove(fileName)
+	}()
+
+	if err != nil {
+		t.Errorf("Failed to create tmp file. Err: %v", err)
+	}
+
+	hook, err := fsrus.NewFilesystemHook(nil, fileName, nil, nil)
+	if err != nil {
+		t.Errorf("Failed to create file system hook: %v", err)
+	}
+
+	logger.AddHook(hook)
+
+	// Log an expected message and a message which should be filtered out.
+	logger.Info(expectedMsg)
+	logger.Debug(expectedMsg)
+	logger.Warning(expectedMsg)
+	logger.Error(expectedMsg)
+
+	scanner := bufio.NewScanner(tmpFile)
+	scanner.Split(bufio.ScanLines)
+
+	numReceivedMsgs := 0
+	for scanner.Scan() {
+		curr := scanner.Text()
+		if strings.Contains(curr, expectedMsg) {
+			numReceivedMsgs++
+		} else {
+			t.Errorf("Unexpected message: %s", curr)
+		}
+	}
+
+	if numReceivedMsgs != 4 {
+		t.Errorf("Didn't receive all of the expected messages. Received: %d", numReceivedMsgs)
+	}
+}
+
 // TestWritingToLog_LevelPathMap tests writing a log entry to
 // a file using level paths
 func TestWritingToLog_LevelPathMap(t *testing.T) {
@@ -18,6 +68,7 @@ func TestWritingToLog_LevelPathMap(t *testing.T) {
 	filteredMsg := "This message should be filtered"
 
 	logger := logrus.New()
+	logger.Level = logrus.DebugLevel
 
 	tmpFile, err := ioutil.TempFile("", "test.txt")
 	fileName := tmpFile.Name()
