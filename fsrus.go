5ad22059cdb1b4d6016b419efe558e257ee32c29
Eric Hotinger <erhoting@microsoft.com>
2018-04-02 13:05:03 -0700

    Initial implementation

--- fsrus.go
+++ fsrus.go
@@ -1,1 +1,128 @@
+// Package fsrus is a file system hook for logrus.
 package fsrus
+
+import (
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
+
+var defaultTextFormatter = &logrus.TextFormatter{DisableColors: true}
+var defaultLevels = logrus.AllLevels
+
+// FsHook represents a filesystem hook.
+type FsHook struct {
+	lock             *sync.Mutex
+	formatter        logrus.Formatter
+	levelPathMap     LevelPathMap
+	defaultLevelPath string
+	levels           []logrus.Level
+}
+
+// LevelPathMap maps a file path to a logrus log level.
+type LevelPathMap map[logrus.Level]string
+
+// NewFilesystemHook returns a filesystem hook.
+// A valid level path map or default path is required.
+func NewFilesystemHook(lMap LevelPathMap, defaultPath string, lvls []logrus.Level, fmt logrus.Formatter) (*FsHook, error) {
+	hook := &FsHook{
+		lock: &sync.Mutex{},
+	}
+
+	if lMap == nil && len(defaultPath) <= 0 {
+		return hook, errors.New("A default path or a level map must be provided")
+	}
+
+	hook.SetLevelPathMap(lMap)
+	hook.SetDefaultLevelPath(defaultPath)
+	hook.SetLevels(lvls)
+	hook.SetFormatter(fmt)
+
+	return hook, nil
+}
+
+// SetFormatter sets the filesystem hook's logrus formatter.
+// If the provided formatter is nil, it will default to a text formatter
+// with colors disabled.
+func (hook *FsHook) SetFormatter(formatter logrus.Formatter) {
+	if formatter == nil {
+		formatter = defaultTextFormatter
+	}
+
+	hook.formatter = formatter
+}
+
+// SetLevels sets the hook's levels to the specified array of logrus levels.
+// These levels determine when the hook should fire.
+func (hook *FsHook) SetLevels(levels []logrus.Level) {
+	if levels == nil {
+		hook.levels = defaultLevels
+	} else {
+		hook.levels = levels
+	}
+}
+
+// SetLevelPathMap sets the hook's level path map.
+func (hook *FsHook) SetLevelPathMap(levelPathMap LevelPathMap) {
+	hook.levelPathMap = levelPathMap
+}
+
+// SetDefaultLevelPath sets the default path for all levels.
+func (hook *FsHook) SetDefaultLevelPath(defaultLevelPath string) {
+	if len(defaultLevelPath) > 0 {
+		hook.defaultLevelPath = defaultLevelPath
+	}
+}
+
+// Fire writes the specified log entry to a file.
+func (hook *FsHook) Fire(entry *logrus.Entry) error {
+	return hook.writeToFile(entry)
+}
+
+// Levels defines which levels should fire the hook.
+func (hook *FsHook) Levels() []logrus.Level {
+	return hook.levels
+}
+
+// writeToFile writes the specified entry to a file
+// using the hook's underlying formatter.
+func (hook *FsHook) writeToFile(entry *logrus.Entry) error {
+	hook.lock.Lock()
+	defer hook.lock.Unlock()
+
+	levelPath, ok := hook.levelPathMap[entry.Level]
+	if !ok {
+		if hook.defaultLevelPath != "" {
+			levelPath = hook.defaultLevelPath
+		} else {
+			return nil
+		}
+	}
+
+	// Try to make the directory for the corresponding level path if it
+	// doesn't exist already and chmod 777.
+	dir := filepath.Dir(levelPath)
+	_ = os.MkdirAll(dir, os.ModePerm)
+
+	// TODO: DESIGN: Some use-cases may want O_SYNC
+	file, err := os.OpenFile(levelPath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		log.Printf("Failed to open file. Path: %s, Err: %v\n", levelPath, err)
+		return err
+	}
+	defer file.Close()
+
+	msg, err := hook.formatter.Format(entry)
+
+	if err != nil {
+		log.Printf("Failed to format entry. Err: %v\n", err)
+		return err
+	}
+
+	file.Write(msg)
+	return nil
+}
--- fsrus_test.go
+++ fsrus_test.go
@@ -1,1 +1,68 @@
 package fsrus_test
+
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/ehotinger/fsrus"
+	"github.com/sirupsen/logrus"
+)
+
+// TestWritingToLog_LevelPathMap tests writing a log entry to
+// a file using level paths
+func TestWritingToLog_LevelPathMap(t *testing.T) {
+	expectedMsg := "This is the expected output"
+	filteredMsg := "This message should be filtered"
+
+	logger := logrus.New()
+
+	tmpFile, err := ioutil.TempFile("", "test.txt")
+	fileName := tmpFile.Name()
+
+	defer func() {
+		tmpFile.Close()
+		os.Remove(fileName)
+	}()
+
+	if err != nil {
+		t.Errorf("Failed to create tmp file. Err: %v", err)
+	}
+
+	levelPathMap := fsrus.LevelPathMap{
+		logrus.InfoLevel: fileName,
+	}
+
+	hook, err := fsrus.NewFilesystemHook(levelPathMap, "", nil, nil)
+	if err != nil {
+		t.Errorf("Failed to create file system hook: %v", err)
+	}
+
+	logger.AddHook(hook)
+
+	// Log an expected message and a message which should be filtered out.
+	logger.Info(expectedMsg)
+	logger.Debug(filteredMsg)
+	logger.Warning(filteredMsg)
+	logger.Error(filteredMsg)
+
+	scanner := bufio.NewScanner(tmpFile)
+	scanner.Split(bufio.ScanLines)
+
+	receivedMsg := false
+	for scanner.Scan() {
+		curr := scanner.Text()
+		if strings.Contains(curr, expectedMsg) {
+			receivedMsg = true
+			t.Log("Received the expected message")
+		} else {
+			t.Errorf("Unexpected message: %s", curr)
+		}
+	}
+
+	if !receivedMsg {
+		t.Errorf("Didn't receive the expected message")
+	}
+}
